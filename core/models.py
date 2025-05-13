95f6ee1cce6985a3e6dfe49083c940c25d64ffa8
kibet <kibetlelgo49@gmail.com>
2025-05-13 11:34:10 +0300

    rest framework

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser:
+class CustomUser(AbstractUser):
     city = models.CharField(max_length=100, blank=True, null=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
